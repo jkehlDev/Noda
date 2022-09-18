0ca25e636c174ac8e84f6b8b12243c820a084b02
jkehlDev <jkehl.dev@gmail.com>
2022-09-18 18:15:54 +0200

    chore(dependencies): add nodemon and configure build and start script

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 export type TestFct = () => void;
 export const testFct: TestFct = () => {
-	//sdfsdfsdf
+	console.log('hello world', process.env.TZ);
 };
+
+testFct();
